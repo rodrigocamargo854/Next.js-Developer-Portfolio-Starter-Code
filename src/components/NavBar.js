9f193b5c9e9845b8ffa8c72f0299bc5bdf0b9995
Rodrigo <rodrigocamargo854@gmai.com>
2023-09-22 08:55:01 -0300

    add styles and Logo and NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Link from "next/link";
+import { Logo } from "./Logo";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span className="h-[1px] inline-block w-0 bg-dark absolute left-0 bottom-0.5 group-hover:w-full transition-[width] ease duration-300">
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+      <nav>
+        <CustomLink title="home" href="/" className="mr-4" />
+        <CustomLink title="About" href="/about" className="mr-4" />
+        <CustomLink title="Projects" href="/projects" className="mr-4" />
+        <CustomLink title="Articles" href="/articles" className="ml-4" />
+      </nav>
+      <nav>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+      </nav>
+
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
